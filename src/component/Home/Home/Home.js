103e88db857121a7495aaec6952dabf0b25a804d
topu12345 <topu15-3604@diu.edu.bd>
2022-11-10 00:58:21 +0600

    banner

--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <Services></Services>
         </div>
     );
 };
---
+++ src/component/Home/Services/ServiceCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
+const ServiceCard = ({ service }) => {
+    const { _id, img, price, name,description } = service;
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure><img src={img} alt="" /></figure>
+         
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{description}</p>
+                <p className='text-2xl text-orange-600 font-semibold'>Price: {price}</p>
+                <div className="card-actions justify-end">
+                    <Link to={`/checkout/${_id}`}>
+                        <button className="btn btn-primary">Checkout</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
