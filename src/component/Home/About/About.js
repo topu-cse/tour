5ff59d04897a68cb4b25bbcca99b57a550bbe8f0
topu12345 <topu15-3604@diu.edu.bd>
2022-11-10 19:23:53 +0600

    review

---
+++ src/component/Home/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <div className='back-img'>
+                <p>LOVE WHERE YOU'RE GOING IN THE PERFECT TIME</p>
+             <h1>a traveling around from place to place</h1>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/component/Home/Home/Home.js
+++ src/component/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from '../About/About';
 import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 
@@ -10,6 +11,7 @@ const Home = () => {
         <div>
            <Banner></Banner>
            <Services></Services>
+           <About></About>
         </div>
     );
 };
